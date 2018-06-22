f63612dd703f52165937db8c0ca34a519b5db205
yangping <office.yangping717@gmail.com>
2018-06-22 10:52:55 +0800

    test
    test

---
+++ web_SAT_training/assets/js/popmodal_javas.js
@@ -0,0 +1,350 @@
+$(function(){
+	$('#popModal_ex1').click(function(){
+		$('#popModal_ex1').popModal({
+			html : $('#content1'),
+			placement : 'bottomLeft',
+			showCloseBut : true,
+			onDocumentClickClose : true,
+			onDocumentClickClosePrevent : '',
+			overflowContent : false,
+			inline : true,
+			asMenu : false,
+			beforeLoadingContent : 'Please, wait...',
+			onOkBut : function() {},
+			onCancelBut : function() {},
+			onLoad : function() {},
+			onClose : function() {}
+		});
+	});
+	
+	$('#popModal_ex2').click(function(){
+		$('#popModal_ex2').popModal({
+			html : $('#content2'),
+			placement : 'bottomLeft',
+			showCloseBut : true,
+			onDocumentClickClose : true,
+			onDocumentClickClosePrevent : '',
+			overflowContent : false,
+			inline : true,
+			asMenu : false,
+			beforeLoadingContent : 'Please, wait...',
+			onOkBut : function() {},
+			onCancelBut : function() {},
+			onLoad : function() {},
+			onClose : function() {}
+		});
+	});
+	
+	$('#popModal_ex3').click(function(){
+		$('#popModal_ex3').popModal({
+			html : $('#content3'),
+			placement : 'bottomLeft',
+			showCloseBut : true,
+			onDocumentClickClose : true,
+			onDocumentClickClosePrevent : '',
+			overflowContent : false,
+			inline : true,
+			asMenu : false,
+			beforeLoadingContent : 'Please, wait...',
+			onOkBut : function() {},
+			onCancelBut : function() {},
+			onLoad : function() {},
+			onClose : function() {}
+		});
+	});
+	
+	$('#popModal_ex4').click(function(){
+		$('#popModal_ex4').popModal({
+			html : $('#content4'),
+			placement : 'bottomLeft',
+			showCloseBut : true,
+			onDocumentClickClose : true,
+			onDocumentClickClosePrevent : '',
+			overflowContent : false,
+			inline : true,
+			asMenu : false,
+			beforeLoadingContent : 'Please, wait...',
+			onOkBut : function() {},
+			onCancelBut : function() {},
+			onLoad : function() {},
+			onClose : function() {}
+		});
+	});
+	
+	$('#popModal_ex11').click(function(){
+		$('#popModal_ex11').popModal({
+			html : $('#content11'),
+			placement : 'bottomLeft',
+			showCloseBut : true,
+			onDocumentClickClose : true,
+			onDocumentClickClosePrevent : '',
+			overflowContent : false,
+			inline : true,
+			asMenu : false,
+			beforeLoadingContent : 'Please, wait...',
+			onOkBut : function() {},
+			onCancelBut : function() {},
+			onLoad : function() {},
+			onClose : function() {}
+		});
+	});
+	
+	$('#popModal_ex12').click(function(){
+		$('#popModal_ex12').popModal({
+			html : $('#content12'),
+			placement : 'bottomLeft',
+			showCloseBut : true,
+			onDocumentClickClose : true,
+			onDocumentClickClosePrevent : '',
+			overflowContent : false,
+			inline : true,
+			asMenu : false,
+			beforeLoadingContent : 'Please, wait...',
+			onOkBut : function() {},
+			onCancelBut : function() {},
+			onLoad : function() {},
+			onClose : function() {}
+		});
+	});
+	
+	$('#popModal_ex13').click(function(){
+		$('#popModal_ex13').popModal({
+			html : $('#content13'),
+			placement : 'bottomLeft',
+			showCloseBut : true,
+			onDocumentClickClose : true,
+			onDocumentClickClosePrevent : '',
+			overflowContent : false,
+			inline : true,
+			asMenu : false,
+			beforeLoadingContent : 'Please, wait...',
+			onOkBut : function() {},
+			onCancelBut : function() {},
+			onLoad : function() {},
+			onClose : function() {}
+		});
+	});
+	
+	$('#popModal_ex14').click(function(){
+		$('#popModal_ex14').popModal({
+			html : $('#content14'),
+			placement : 'bottomLeft',
+			showCloseBut : true,
+			onDocumentClickClose : true,
+			onDocumentClickClosePrevent : '',
+			overflowContent : false,
+			inline : true,
+			asMenu : false,
+			beforeLoadingContent : 'Please, wait...',
+			onOkBut : function() {},
+			onCancelBut : function() {},
+			onLoad : function() {},
+			onClose : function() {}
+		});
+	});
+	
+	$('#popModal_ex21').click(function(){
+		$('#popModal_ex21').popModal({
+			html : $('#content21'),
+			placement : 'bottomLeft',
+			showCloseBut : true,
+			onDocumentClickClose : true,
+			onDocumentClickClosePrevent : '',
+			overflowContent : false,
+			inline : true,
+			asMenu : false,
+			beforeLoadingContent : 'Please, wait...',
+			onOkBut : function() {},
+			onCancelBut : function() {},
+			onLoad : function() {},
+			onClose : function() {}
+		});
+	});
+	
+	$('#popModal_ex22').click(function(){
+		$('#popModal_ex22').popModal({
+			html : $('#content22'),
+			placement : 'bottomLeft',
+			showCloseBut : true,
+			onDocumentClickClose : true,
+			onDocumentClickClosePrevent : '',
+			overflowContent : false,
+			inline : true,
+			asMenu : false,
+			beforeLoadingContent : 'Please, wait...',
+			onOkBut : function() {},
+			onCancelBut : function() {},
+			onLoad : function() {},
+			onClose : function() {}
+		});
+	});
+	
+	$('#popModal_ex23').click(function(){
+		$('#popModal_ex23').popModal({
+			html : $('#content23'),
+			placement : 'bottomLeft',
+			showCloseBut : true,
+			onDocumentClickClose : true,
+			onDocumentClickClosePrevent : '',
+			overflowContent : false,
+			inline : true,
+			asMenu : false,
+			beforeLoadingContent : 'Please, wait...',
+			onOkBut : function() {},
+			onCancelBut : function() {},
+			onLoad : function() {},
+			onClose : function() {}
+		});
+	});
+	
+	$('#popModal_ex24').click(function(){
+		$('#popModal_ex24').popModal({
+			html : $('#content24'),
+			placement : 'bottomLeft',
+			showCloseBut : true,
+			onDocumentClickClose : true,
+			onDocumentClickClosePrevent : '',
+			overflowContent : false,
+			inline : true,
+			asMenu : false,
+			beforeLoadingContent : 'Please, wait...',
+			onOkBut : function() {},
+			onCancelBut : function() {},
+			onLoad : function() {},
+			onClose : function() {}
+		});
+	});
+	
+	$('#popModal_ex100').hover(function(){
+		$('#popModal_ex100').popModal({
+			html : $('#content100'),
+			placement : 'bottomLeft',
+			showCloseBut : true,
+			onDocumentClickClose : true,
+			onDocumentClickClosePrevent : '',
+			overflowContent : false,
+			inline : true,
+			asMenu : false,
+			beforeLoadingContent : 'Please, wait...',
+			onOkBut : function() {},
+			onCancelBut : function() {},
+			onLoad : function() {},
+			onClose : function() {}
+		});
+	});
+	
+	$('#popModal_ex101').hover(function(){
+		$('#popModal_ex101').popModal({
+			html : $('#content101'),
+			placement : 'bottomLeft',
+			showCloseBut : true,
+			onDocumentClickClose : true,
+			onDocumentClickClosePrevent : '',
+			overflowContent : false,
+			inline : true,
+			asMenu : false,
+			beforeLoadingContent : 'Please, wait...',
+			onOkBut : function() {},
+			onCancelBut : function() {},
+			onLoad : function() {},
+			onClose : function() {}
+		});
+	});
+	
+	$('#popModal_ex102').hover(function(){
+		$('#popModal_ex102').popModal({
+			html : $('#content102'),
+			placement : 'bottomLeft',
+			showCloseBut : true,
+			onDocumentClickClose : true,
+			onDocumentClickClosePrevent : '',
+			overflowContent : false,
+			inline : true,
+			asMenu : false,
+			beforeLoadingContent : 'Please, wait...',
+			onOkBut : function() {},
+			onCancelBut : function() {},
+			onLoad : function() {},
+			onClose : function() {}
+		});
+	});
+	
+	$('#popModal_ex103').hover(function(){
+		$('#popModal_ex103').popModal({
+			html : $('#content103'),
+			placement : 'bottomLeft',
+			showCloseBut : true,
+			onDocumentClickClose : true,
+			onDocumentClickClosePrevent : '',
+			overflowContent : false,
+			inline : true,
+			asMenu : false,
+			beforeLoadingContent : 'Please, wait...',
+			onOkBut : function() {},
+			onCancelBut : function() {},
+			onLoad : function() {},
+			onClose : function() {}
+		});
+	});
+	
+//	$('#popModal_ex2').click(function(){
+//		$('#popModal_ex2').popModal({
+//			html : function(callback) {
+//				$.ajax({url:'ajax.html'}).done(function(content){
+//					callback(content);
+//				});
+//			}
+//		});
+//	});
+	
+//	$('#popModal_ex3').click(function(){
+//		$('#popModal_ex3').popModal({
+//			html : $('#content3'),
+//			placement : 'bottomLeft',
+//			asMenu : true
+//		});
+//	});
+	
+	$('#notifyModal_ex1').click(function(){
+		$('#content2').notifyModal({
+			duration : 2500,
+			placement : 'center',
+			overlay : true,
+			type : 'notify',
+			icon: false,
+			onClose : function() {}
+		});
+	});
+	
+	$('#dialogModal_ex1').click(function(){
+		$('.dialog_content').dialogModal({
+			topOffset: 0,
+			top: 0,
+			type: '',
+			onOkBut: function() {},
+			onCancelBut: function() {},
+			onLoad: function(el, current) {},
+			onClose: function() {},
+			onChange: function(el, current) {
+				if(current == 3){
+					el.find('.dialogModal_header span').text('Page 3');
+					$.ajax({url:'ajax.html'}).done(function(content){
+						el.find('.dialogModal_content').html(content);
+					});
+				}
+			}
+		});
+	});
+	
+	$('#confirmModal_ex1').click(function(){
+		$('#confirm_content').confirmModal({
+			topOffset: 0,
+			top: 0,
+			onOkBut: function() {},
+			onCancelBut: function() {},
+			onLoad: function() {},
+			onClose: function() {}
+		});
+	});
+	
+});
