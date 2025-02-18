c37397f626bbbf46196c1cbc429d17b4ba8ca4c0
YangPing0717 <office.yangping717@gmail.com>
2025-02-18 17:51:40 +0800

    BA

--- web_EZcon/js/script_slick.js
+++ web_EZcon/js/script_slick.js
@@ -32,3 +32,33 @@ function carousel() {
 }
 
 carousel();
+
+function carouselpad() {
+  $.js('timeline-carousel-pad').slick({
+    infinite: false,
+    arrows: true,
+    dots: false,
+    autoplay: true,
+    // autoplaySpeed: 3000,
+    speed: 300,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    // nextArrow: '.next_caro',
+    // prevArrow: '.previous_caro',
+    // prevArrow: $('.prev'),
+    // nextArrow: $('.next'),
+    arrows: true,
+    prevArrow: '<div class="slick-prev"><i class="fa fa-angle-left" aria-hidden="true"></i></div>',
+    nextArrow: '<div class="slick-next"><i class="fa fa-angle-right" aria-hidden="true"></i></div>',
+    responsive: [
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }]
+  });
+}
+
+carouselpad();
