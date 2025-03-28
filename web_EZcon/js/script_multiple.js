161ddfbdad9e7a4dc1f2321a417880d8aa8ece9e
YangPing0717 <office.yangping717@gmail.com>
2025-03-28 13:06:16 +0800

    BG

---
+++ web_EZcon/js/script_multiple.js
@@ -0,0 +1,34 @@
+const baseColor = 0x1a59;
+const backgroundColor = 0x0d2324;
+const vantaOptions = [
+{
+  el: "#vanta1",
+  yOffset: 0.5,
+  size: 2,
+  amplitudeFactor: 1.5 },
+
+{
+  el: "#vanta2",
+  xOffset: -0.4,
+  yOffset: -0.2 },
+
+{
+  el: "#vanta3",
+  xOffset: 0.5,
+  yOffset: -0.2,
+  size: 1.8,
+  amplitudeFactor: 2 }];
+
+
+
+vantaOptions.map(options => {
+  return VANTA.HALO({
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    baseColor: baseColor,
+    backgroundColor: backgroundColor,
+    size: 0.8,
+    ...options });
+
+});
